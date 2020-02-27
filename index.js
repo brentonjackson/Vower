f4a028e42a2163c69bb009fa27662c60f48614f9
Brenton <bjackson71@gmail.com>
2020-02-27 08:41:22 -0500

    removed console.logs from js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ let submit = document.getElementById('submit');
 // Add eventlistener to prevent post on click
 submit.addEventListener("click", function(stuff) {
 	stuff.preventDefault();
-	console.log("Prevented POST");
 });
 
 
@@ -18,12 +17,10 @@ let mybutton = document.getElementById("myBtn");
 // Show button when user scrolls down 50px, otherwise hide
 function scrollFunction () {
 	if (document.body.scrollTop > 50) {
-		console.log("showing button");
 		mybutton.style.display = "block";
 		mybutton.style.position = "fixed";
 	}
 	else {
-		console.log("hiding button");
 		mybutton.style.display = "none";
 	}
 }
@@ -35,7 +32,6 @@ document.body.addEventListener("scroll", scrollFunction);
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  console.log("scrolling to top");
 }
 
 // Add click functionality to button
